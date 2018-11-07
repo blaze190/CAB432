494d81edb426f9d3b2ca49c08350aa69f73b76f2
Unknown <marcuswatkins211@gmail.com>
2018-11-07 16:42:33 +1000

    well then

---
+++ 2ndass/2ndass/2ndass/config.js
@@ -0,0 +1,15 @@
+const config = {
+	twitter: {
+		consumer:{
+			key: '0XGYEQDzz7YJ8LmM1qBbqaMOm',
+  			secret: '78QBUfeHej5k9lGdcMzkkszua0aQmauul0zptq8AtM5z7oIOr6'
+		},
+		access:{
+			key: '342049670-UpnANf8IAntVJtJZ8nWCX154H7MZv4EnGBppZDsV',
+  			secret: 'pqYlgyKpGUWI2Uj3VgLvE2SZZLkzfNtHoJvZ3aMMu0bhS'
+		},
+
+	}
+};
+
+module.exports = config;
